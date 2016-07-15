2ba065d21773da1d9bfda17deeffea49286ecee6
Sven Kästel <osis@outlook.de>
2016-07-15 14:43:44 +0200

    dbread* mit callback
    Die dbread, dbreadTo, dbreadFrom und dbreadRange wurden mit callback für onsucces und onerror versehen.

--- idb.js
+++ idb.js
@@ -1,17 +1,9 @@
-//Config
-//Testdb
-//var idb_db="testdb";			//<= Databasename
-//var idb_os="testtabelle";		//<= Tablename
-
-
 var idb = (function(){
 document.addEventListener("DOMContentLoaded", function(){
  
     if("indexedDB" in window) {
-//        console.log("YES!!! I CAN DO IT!!! WOOT!!!");
 
 var openRequest = indexedDB.open(idb_db,1);
-//upgrade
 openRequest.onupgradeneeded = function(e){
 var thisDB = e.target.result;
 if(!thisDB.objectStoreNames.contains(idb_os)){
@@ -31,19 +23,15 @@ return{
 	dbwrite : function(rdb){
 		var openRequest = openDB();
 		openRequest.onsuccess = function(e){
-//			console.log(rdb);
 			var db = e.target.result;
 			var store = getStore(db,"readwrite");
 			var count = store.count();
 			count.onsuccess = function(e){
 				res = e.target.result+1;
-//			console.log(res);	
 			var request = store.add(rdb,res);
 			request.onerror = function(e){
-//				console.log("Error",e.target.error.name);
 			}
 			request.onsuccess = function(e){
-//				console.log("Daten gespeichert");
 			}
 			}
 		}
@@ -52,40 +40,24 @@ return{
 	dbupdate : function(rdb,key){
 		var openRequest = openDB();
 		openRequest.onsuccess = function(e){
-//			console.log(rdb);
 			var db = e.target.result;
 			var store = getStore(db,"readwrite");
 			var request = store.put(rdb,key);
 			request.onerror = function(e){
-//				console.log("Error",e.target.error.name);
 			}
 			request.onsuccess = function(e){
-//				console.log("Daten gespeichert");
 			}
 		}
 	},
 	
-	dbread : function(selector,type,rdb){
-//		console.log(rdb);
+	dbread : function(rdb,onsu,oner){
 		var openRequest = openDB();
-		openRequest.onsuccess = function(e){
-			var db = e.target.result;
-			var store = getStore(db,"readonly");
-			var request = store.get(Number(rdb));
-			request.onsuccess = function(e){
-				var res = e.target.result;
-				console.dir(res);
-				if(res){
-				output(res.value,selector,type);
-					for(var field in res){
-//						console.log(field + " = " + res[field]);	
-					}
-				}
-			}
+		openRequest.onsuccess = function(evt){
+			rdb.map((e) => readline(e,evt,onsu,oner));
 		}
 	},
 	
-	dbreadFrom : function(selector,type,rdb,direction){
+	dbreadFrom : function(rdb,onsu,oner,direction){
 		direction = direction ? direction : 'next';
 		var openRequest = openDB();
 		openRequest.onsuccess = function(e){
@@ -93,18 +65,12 @@ return{
 			var store = getStore(db,"readonly");
 			var range = IDBKeyRange.lowerBound(rdb);
 			var cursor = store.openCursor(range,direction);
-			cursor.onsuccess = function(e){
-				var res = e.target.result;
-				if(res){
-				output(res.value,selector,type);
-//				console.dir(res.value);
-				res.continue();
-				}
-			}
+			cursor.onsuccess = onsu;
+			cursor.onerror = oner;
 		}
 	},
 	
-	dbreadTo : function(selector,type,rdb,direction){
+	dbreadTo : function(rdb,onsu,oner,direction){
 		direction = direction ? direction : 'next';
 		var openRequest = openDB();
 		openRequest.onsuccess = function(e){
@@ -112,18 +78,12 @@ return{
 			var store = getStore(db,"readonly");
 			var range = IDBKeyRange.upperBound(rdb);
 			var cursor = store.openCursor(range,direction);
-			cursor.onsuccess = function(e){
-				var res = e.target.result;
-				if(res){
-				output(res.value,selector,type);
-//				console.dir(res.value);
-				res.continue();
-				}
-			}
+			cursor.onsuccess = onsu;
+			cursor.onerror = oner;
 		}
 	},
 	
-	dbreadRange : function(selector,type,f,t,direction){
+	dbreadRange : function(f,t,onsu,oner,direction){
 		direction = direction ? direction : 'next';
 		var openRequest = openDB()
 		openRequest.onsuccess = function(e){
@@ -131,14 +91,8 @@ return{
 			var store = getStore(db,"readonly");
 			var range = IDBKeyRange.bound(f, t);
 			var cursor = store.openCursor(range,direction);
-			cursor.onsuccess = function(e){
-				var res = e.target.result;
-				if(res){
-				output(res.value,selector,type);
-//				console.dir(res.value);
-				res.continue();
-				}
-			}
+			cursor.onsuccess = onsu;
+			cursor.onerror = oner;
 		}
 	},
 	
@@ -164,29 +118,24 @@ return{
 		var transaction = db.transaction([idb_os],connect);
 		return transaction.objectStore(idb_os);
 	}
-
- 	function output(data,selector,type){
-		type == "html" ? toHtml(data,selector):
-		type == "value" ? toValue(data,selector):
-		type == "text" ? toText(data,selector):
-		type == "cd" ? toConsoleD(data,selector):
-		type == "cl" ? toConsoleL(data,selector): console.log('Keine Ausgabe methode vorhanden!');
-	}
 	
-	function toHtml(data,selector){
-		$(selector).html(data);
+	function readline(key,evt,onsu,oner){
+			var db = evt.target.result;
+			var store = getStore(db,"readonly");
+			var request = store.get(Number(key));
+			request.onsuccess = onsu;
+/*				var res = e.target.result;
+//				console.dir(res);
+				if(res){
+					onsu;
+//				output(res.value,selector,type);
+					for(var field in res){
+//						console.log(field + " = " + res[field]);	
+					}
+				}
+			}*/
+			request.onerror = oner;
 	}
-	function toValue(data,selector){
-		$(selector).value(data);
-	}
-	function toText(data,selector){
-		$(selector).text(data);
-	}
-	function toConsoleD(data,selector){
-		console.dir(data);
-	}
-	function toConsoleL(data,selector){
-		console.log(data);
-	}
+
 })();
 
